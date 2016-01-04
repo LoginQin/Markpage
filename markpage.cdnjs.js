4c9bc0d34b9fcfa54f934439550ff9d32eaa9533
qinwei <qinwei081@foxmail.com>
2016-01-04 18:21:21 +0800

    change \t to blank

--- markpage.cdnjs.js
+++ markpage.cdnjs.js
@@ -37,60 +37,60 @@
  * Thansk for those open source libary bellow, and you can change the url if you want, such as from cdnjs 
  **/
 var ResConfig = {
-	marx_css: 'https://cdnjs.cloudflare.com/ajax/libs/marx/1.3.0/marx.min.css',
-	mermaid_css: 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/0.5.5/mermaid.min.css',
+    marx_css: 'https://cdnjs.cloudflare.com/ajax/libs/marx/1.3.0/marx.min.css',
+    mermaid_css: 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/0.5.5/mermaid.min.css',
 
-	zepto_js: 'https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js',
-	showdown_js: 'https://cdnjs.cloudflare.com/ajax/libs/showdown/1.3.0/showdown.min.js',
-	highlight_js: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/highlight.min.js',
-	mermaid_js: 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/0.5.5/mermaid.min.js'
+    zepto_js: 'https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js',
+    showdown_js: 'https://cdnjs.cloudflare.com/ajax/libs/showdown/1.3.0/showdown.min.js',
+    highlight_js: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/highlight.min.js',
+    mermaid_js: 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/0.5.5/mermaid.min.js'
 };
 (function() {
-	function markpage() {
-		var self = this;
-		var resourceNumExpect = 4; // expect load js resource number
-		var loadedResource = 0; // current loaded
-		var onLoadCallback = function() {};
+    function markpage() {
+        var self = this;
+        var resourceNumExpect = 4; // expect load js resource number
+        var loadedResource = 0; // current loaded
+        var onLoadCallback = function() {};
 
-		function checkResourceLoad() {
-			loadedResource++;
-			if (loadedResource == resourceNumExpect) {
-				resourceOnload();
-			}
-		}
+        function checkResourceLoad() {
+            loadedResource++;
+            if (loadedResource == resourceNumExpect) {
+                resourceOnload();
+            }
+        }
 
-		this.mkdom = function(name, attr) {
-			attr = attr || {};
-			var dom = document.createElement(name);
-			for (var i in attr) {
-				dom.setAttribute(i, attr[i])
-			}
-			return dom;
-		};
+        this.mkdom = function(name, attr) {
+            attr = attr || {};
+            var dom = document.createElement(name);
+            for (var i in attr) {
+                dom.setAttribute(i, attr[i])
+            }
+            return dom;
+        };
 
-		this.addChildNode = function(tagName, childNode) {
-			var head = document.getElementsByTagName(tagName)[0];
-			head.appendChild(childNode);
-		};
+        this.addChildNode = function(tagName, childNode) {
+            var head = document.getElementsByTagName(tagName)[0];
+            head.appendChild(childNode);
+        };
 
-		this.loadcss = function(link) {
-			var linkNode = self.mkdom('link', {
-				rel: 'stylesheet',
-				type: "text/css",
-				href: link
-			});
-			self.addChildNode('head', linkNode);
-		}
-		this.loadjs = function(src, onloadCallback) {
-			var jsnode = self.mkdom('script', {
-				type: 'text/javascript',
-				src: src
-			});
-			self.addChildNode('body', jsnode);
-			jsnode.onload = onloadCallback;
-		}
+        this.loadcss = function(link) {
+            var linkNode = self.mkdom('link', {
+                rel: 'stylesheet',
+                type: "text/css",
+                href: link
+            });
+            self.addChildNode('head', linkNode);
+        }
+        this.loadjs = function(src, onloadCallback) {
+            var jsnode = self.mkdom('script', {
+                type: 'text/javascript',
+                src: src
+            });
+            self.addChildNode('body', jsnode);
+            jsnode.onload = onloadCallback;
+        }
 
-		this.loadDocCss = function() {
+        this.loadDocCss = function() {
             var css = [
                'h1 {padding:0 0 10px 0;}'
                ,'.docmenu { position:relative;float:left; height:100%; width:20%; }'
@@ -109,140 +109,140 @@ var ResConfig = {
                , '.docmenu .menu { position:relative; width:100%;}'
                , '}'
             ];
-			// can't not use $ before zepto/jquery load
-			var style = self.mkdom('style', {
-				type: 'text/css'
-			});
-			style.appendChild(document.createTextNode(css.join('')));
-			self.addChildNode('head', style);
-		}
+            // can't not use $ before zepto/jquery load
+            var style = self.mkdom('style', {
+                type: 'text/css'
+            });
+            style.appendChild(document.createTextNode(css.join('')));
+            self.addChildNode('head', style);
+        }
 
-	   /**
+       /**
         * This function will invoke when all resource loaded
         */
-		function resourceOnload() {
-			initShowdownExt();
-			hljs.initHighlightingOnLoad();
-			mermaidAPI.initialize({
-				startOnLoad: false
-			});
-			mermaid.parseError = function(err, hash) {
-				console.error(err);
-			};
+        function resourceOnload() {
+            initShowdownExt();
+            hljs.initHighlightingOnLoad();
+            mermaidAPI.initialize({
+                startOnLoad: false
+            });
+            mermaid.parseError = function(err, hash) {
+                console.error(err);
+            };
 
-			onLoadCallback.call(self);
-		}
+            onLoadCallback.call(self);
+        }
 
-		this.loadResource = function(cb) {
-			self.loadcss(ResConfig.marx_css);
-			self.loadcss(ResConfig.mermaid_css);
-			self.loadDocCss();
-			// expect 3 js resource to load
-			self.loadjs(ResConfig.zepto_js, checkResourceLoad);
-			self.loadjs(ResConfig.showdown_js, checkResourceLoad);
-			self.loadjs(ResConfig.highlight_js, checkResourceLoad);
-			self.loadjs(ResConfig.mermaid_js, checkResourceLoad);
-			if (typeof cb == 'function') {
-				onLoadCallback = cb;
-			}
-		}
+        this.loadResource = function(cb) {
+            self.loadcss(ResConfig.marx_css);
+            self.loadcss(ResConfig.mermaid_css);
+            self.loadDocCss();
+            // expect 3 js resource to load
+            self.loadjs(ResConfig.zepto_js, checkResourceLoad);
+            self.loadjs(ResConfig.showdown_js, checkResourceLoad);
+            self.loadjs(ResConfig.highlight_js, checkResourceLoad);
+            self.loadjs(ResConfig.mermaid_js, checkResourceLoad);
+            if (typeof cb == 'function') {
+                onLoadCallback = cb;
+            }
+        }
 
-		self.onload = function(cb) {
-			onLoadCallback = cb;
-		}
+        self.onload = function(cb) {
+            onLoadCallback = cb;
+        }
 
-		this.render = function(selector) {
-			var $doc = $(selector).hide();
-			var theme = $doc.attr('data-highlight-theme') || 'railscasts';
-			self.loadcss('http://cdn.bootcss.com/highlight.js/8.9.1/styles/' + theme + '.min.css');
-			$('body').append('<div class="docmenu"></div><main><section class="docbody"></section></main><footer>Render By MarkPage</footer>');
-			$('#markpage').hide();
-			var text = $doc.html();
-			text = text.replace(/&gt;/ig, ">");
-			text = text.replace(/&lt;/ig, "<");
-			text = text.replace(/&amp;/ig, "&");
-		/**
+        this.render = function(selector) {
+            var $doc = $(selector).hide();
+            var theme = $doc.attr('data-highlight-theme') || 'railscasts';
+            self.loadcss('http://cdn.bootcss.com/highlight.js/8.9.1/styles/' + theme + '.min.css');
+            $('body').append('<div class="docmenu"></div><main><section class="docbody"></section></main><footer>Render By MarkPage</footer>');
+            $('#markpage').hide();
+            var text = $doc.html();
+            text = text.replace(/&gt;/ig, ">");
+            text = text.replace(/&lt;/ig, "<");
+            text = text.replace(/&amp;/ig, "&");
+        /**
          * The Document of showdownjs At: https://github.com/showdownjs/showdown
          */
-			var converter = new showdown.Converter({
-				tables: true,
-				//table support
-				tasklists: true,
-				//tasklists support
-				strikethrough: true,
-				// Enable support for strikethrough syntax. ~~HH~~
-				literalMidWordUnderscores: true,
-				// ignore `list_by_name` transfer to <em> or <strong> 
-				omitExtraWLInCodeBlocks: true,
-				parseImgDimensions: true,
-				ghCodeBlocks: true,
-				extensions: ['markpage']
-			}),
-			html = converter.makeHtml(text);
+            var converter = new showdown.Converter({
+                tables: true,
+                //table support
+                tasklists: true,
+                //tasklists support
+                strikethrough: true,
+                // Enable support for strikethrough syntax. ~~HH~~
+                literalMidWordUnderscores: true,
+                // ignore `list_by_name` transfer to <em> or <strong> 
+                omitExtraWLInCodeBlocks: true,
+                parseImgDimensions: true,
+                ghCodeBlocks: true,
+                extensions: ['markpage']
+            }),
+            html = converter.makeHtml(text);
 
-			//render mermaid charts
-			$('.docbody').append(html).find('.mermaid').each(function(i, el) {
-				var $this = $(el);
-				var cb = function(svgGraph) {
-					var $svg = $('<div />').html(svgGraph).find('svg');
-					$this.html(svgGraph);
+            //render mermaid charts
+            $('.docbody').append(html).find('.mermaid').each(function(i, el) {
+                var $this = $(el);
+                var cb = function(svgGraph) {
+                    var $svg = $('<div />').html(svgGraph).find('svg');
+                    $this.html(svgGraph);
 
-					//resize height to match box
-					var height = parseInt($svg.attr('viewBox').split(/\s+/)[3] || 400); //data like: -50 -10 750 347
-					$this.css('height', (height + 50) + 'px');
-				};
-				var data = $this.text().replace(/^\s+/, "");
-				if (mermaidAPI.parse(data)) {
-					var graphId = 'graph-' + i;
-					mermaidAPI.render(graphId, data, cb, el);
-				} else {
-					$this.append('<p style="color:red">Mermaid syntax has error, you can open the console pannel(F12) to view error logger!</p>');
-				}
-			});
-		}
-	}
+                    //resize height to match box
+                    var height = parseInt($svg.attr('viewBox').split(/\s+/)[3] || 400); //data like: -50 -10 750 347
+                    $this.css('height', (height + 50) + 'px');
+                };
+                var data = $this.text().replace(/^\s+/, "");
+                if (mermaidAPI.parse(data)) {
+                    var graphId = 'graph-' + i;
+                    mermaidAPI.render(graphId, data, cb, el);
+                } else {
+                    $this.append('<p style="color:red">Mermaid syntax has error, you can open the console pannel(F12) to view error logger!</p>');
+                }
+            });
+        }
+    }
     /**
      * init showdown ext after load showdown.js
      */
-	function initShowdownExt() {
-		var markdownMarkpageExt = function(converter) {
-			return [{
-				type: 'output',
-				filter: function(text) {
-					var $ext = $('<div>' + text + '</div>');
-					$ext.find('pre').addClass('prettyprint');
-					var $root = $('<ul class="menu H1" />');
-					var $docmenu = $('.docmenu');
-					$docmenu.append($root);
+    function initShowdownExt() {
+        var markdownMarkpageExt = function(converter) {
+            return [{
+                type: 'output',
+                filter: function(text) {
+                    var $ext = $('<div>' + text + '</div>');
+                    $ext.find('pre').addClass('prettyprint');
+                    var $root = $('<ul class="menu H1" />');
+                    var $docmenu = $('.docmenu');
+                    $docmenu.append($root);
 
-					$ext.find('h1,h2,h3,h4').each(function(i, el) {
-						var $el = $(el);
-						var tagName = $el[0].tagName;
-						var tagNum = tagName.replace('H', '');
-						var linkHtml = '<a href="#' + $el.attr('id') + '" >' + $el.html() + '</a>';
-						var parentTag = [];
-						for (var i = 1; i <= tagNum; i++) {
-							parentTag.push('.H' + i);
-						}
-						var $parentUL = $docmenu.find(parentTag.join(',')).last();
-						var $li = $('<li />').html(linkHtml);
-						$li.append('<ul class="H' + (++tagNum) + '" />');
-						$parentUL.append($li);
-					});
-					return $ext.html();
-				}
-			}];
-		};
+                    $ext.find('h1,h2,h3,h4').each(function(i, el) {
+                        var $el = $(el);
+                        var tagName = $el[0].tagName;
+                        var tagNum = tagName.replace('H', '');
+                        var linkHtml = '<a href="#' + $el.attr('id') + '" >' + $el.html() + '</a>';
+                        var parentTag = [];
+                        for (var i = 1; i <= tagNum; i++) {
+                            parentTag.push('.H' + i);
+                        }
+                        var $parentUL = $docmenu.find(parentTag.join(',')).last();
+                        var $li = $('<li />').html(linkHtml);
+                        $li.append('<ul class="H' + (++tagNum) + '" />');
+                        $parentUL.append($li);
+                    });
+                    return $ext.html();
+                }
+            }];
+        };
 
-		// Client-side export
-		if (typeof window !== 'undefined' && window.showdown && window.showdown.extensions) {
-			window.showdown.extensions.markpage = markdownMarkpageExt;
-		}
-	};
+        // Client-side export
+        if (typeof window !== 'undefined' && window.showdown && window.showdown.extensions) {
+            window.showdown.extensions.markpage = markdownMarkpageExt;
+        }
+    };
 
-	new markpage().loadResource(function() {
-		this.render('#markpage');
-	});
+    new markpage().loadResource(function() {
+        this.render('#markpage');
+    });
 
 })();
 
